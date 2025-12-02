7bc6ae4fac788acd5b808356c7cc7434d1bb95e2
David Heinemeier Hansson <david@hey.com>
2025-12-02 15:11:59 +0100

    Polish

--- app/controllers/my/access_tokens_controller.rb
+++ app/controllers/my/access_tokens_controller.rb
@@ -1,34 +1,37 @@
 class My::AccessTokensController < ApplicationController
   def index
-    @access_tokens = Current.identity.access_tokens.order(created_at: :desc)
+    @access_tokens = my_access_tokens.order(created_at: :desc)
   end
 
   def show
-    access_token_id = verifier.verify(params[:id])
-    @access_token = Current.identity.access_tokens.find(access_token_id)
+    @access_token = my_access_tokens.find(verifier.verify(params[:id]))
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to my_access_tokens_path, alert: "Token is no longer visible"
   end
 
   def new
-    @access_token = Current.identity.access_tokens.new
+    @access_token = my_access_tokens.new
   end
 
   def create
-    access_token = Current.identity.access_tokens.create!(access_token_params)
+    access_token = my_access_tokens.create!(access_token_params)
     expiring_id = verifier.generate access_token.id, expires_in: 10.seconds
 
     redirect_to my_access_token_path(expiring_id)
   end
 
   def destroy
-    Current.identity.access_tokens.find(params[:id]).destroy!
+    my_access_tokens.find(params[:id]).destroy!
     redirect_to my_access_tokens_path
   end
 
   private
+    def my_access_tokens
+      Current.identity.access_tokens
+    end
+
     def access_token_params
-      params.expect(access_token: [ :description, :permission ])
+      params.expect(access_token: %i[ description permission ])
     end
 
     def verifier
